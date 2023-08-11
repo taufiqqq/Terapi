8bb6ecc9267a5b445fb078f5b8e8bb82d2a6f61f
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-08-11 14:45:07 +0800

    Update main.dart

--- terapi/lib/main.dart
+++ terapi/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:terapi/pages/login_page.dart';
-import 'package:terapi/pages/home_page.dart';
+import 'package:terapi/pages/user/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
